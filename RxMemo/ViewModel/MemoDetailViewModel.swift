7a7f9488c1fff36e2c98285d1cde9ffafd775e96
SangJLee1103 <jesusfollow1103@naver.com>
2024-03-03 21:13:37 +0900

    feat: CoreData 적용

--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -12,7 +12,7 @@ import Action
 
 class MemoDetailViewModel: CommonViewModel {
     
-    let memo: Memo
+    var memo: Memo
     
     private var formatter: DateFormatter = {
         let f = DateFormatter()
@@ -42,6 +42,7 @@ class MemoDetailViewModel: CommonViewModel {
     func performUpdate(memo: Memo) -> Action<String, Void> {
         return Action { input in
             self.storage.update(memo: memo, content: input)
+                .do(onNext: { self.memo = $0 })
                 .map { [$0.content, self.formatter.string(from: $0.insertDate)] }
                 .bind(onNext: { self.contens.onNext($0) })
                 .disposed(by: self.rx.disposeBag)
